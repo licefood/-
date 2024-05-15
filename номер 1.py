f64fc4c43c7cb624cb00bf4be48d4a6dca20f0ec
licefood <krav4enko.dmitryi@gmail.com>
2024-05-15 19:26:05 +0300

    Add files via upload

---
+++ номер 1.py
@@ -0,0 +1,28 @@
+import json
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def f(x, A):
+    return 0.5+(np.sin(x**2-A**2)**2)/(1+0.001*(x**2+A**2))
+
+
+A = 0
+x = np.linspace(-10, 10, 400)
+y = f(x, A)
+data = [{"x": x, "y": y} for x, y in zip(x, y)]
+
+if not os.path.exists('results'):
+    os.makedirs('results')
+
+with open('results/values.json', 'w') as file:
+    json.dump(data, file)
+plt.figure(figsize=(16, 9))
+plt.plot(x, y, label='f(x)', color='r')
+plt.title('График')
+plt.xlabel('x')
+plt.ylabel('f(x)')
+plt.grid(True)
+plt.legend()
+plt.show()
